86d2775cb682e832d0e95c3ec453d3c89273924b
Benoît <khertan@khertan.net>
2015-12-16 06:58:43 +0100

    add build script and thanks section

---
+++ scripts/build_release_sign_align.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Android
+ionic build --release android
+jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 -keystore google-store.keystore ~/Projects/ForRunners/platforms/android/build/outputs/apk/android-release-unsigned.apk ForRunners
+rm net.khertan.forrunners.apk
+~/Android/Sdk/build-tools/21.1.2/zipalign -v 4 ./platforms/android/build/outputs/apk/android-release-unsigned.apk net.khertan.forrunners.apk
+
+# FirefoxOS
+cordova prepare
+ionic build --release firefoxos
