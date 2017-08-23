f74a8d1022227413fcd793dc2c88f8cca6ca6f2f
Benoit HERVIER <bhervier@chronolife.net>
2017-08-23 17:18:14 +0200

    iOS try

--- platforms/ios/www/js/services.js
+++ platforms/ios/www/js/services.js
@@ -1,5 +1,32 @@
 angular.module('app.services', [])
 
+// Service to communicate with Nominatim OpenStreetMap API.
+.factory('$nominatim', function($q, $http) {
+    'use strict';
+    var API_ROOT = 'http://nominatim.openstreetmap.org';
+
+    this.byLocation = function(coords) {
+        var deferred = $q.defer();
+
+        if ((coords.latitude === undefined) || (coords.longitude === undefined)) {
+          deferred.reject('Undefined coords');          
+        }
+
+        $http.jsonp(API_ROOT + '/reverse?format=json&lat='+coords.latitude+'&lon='+coords.longitude+'&zoom=10&addressdetails=1&json_callback=JSON_CALLBACK').then(function(response) {
+            var statusCode = parseInt(response.status, 10);
+            if (statusCode === 200) {
+                deferred.resolve(response.data.address.city);
+            } else {
+                deferred.reject(response.data.message);
+            }
+        }, function(error) {
+            deferred.reject(error);
+        });
+        return deferred.promise;
+    };
+    return this;
+})
+
 
 // Service to communicate with OpenWeatherMap API.
 .factory('$weather', function($q, $http) {
@@ -97,7 +124,7 @@ angular.module('app.services', [])
         saveToFile: function(session, app_data_path) {
           var deferred = $q.defer();
           var filename = session.recclicked.toString() + '.json';
-          var path = app_data_pathe;
+          var path = app_data_path;
           if (window.device.platform === 'browser') {
             setTimeout(function(){
               localStorage.setItem(filename, JSON.stringify(session));
